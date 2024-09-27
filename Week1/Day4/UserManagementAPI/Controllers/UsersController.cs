0adb545bbdc5c371ce61bd24769ce96dedf8ea0a
CodeAdmirer <codeadmirer@gmail.com>
2024-09-28 01:25:18 +0530

    Aded the User Management API application code

---
+++ Week1/Day4/UserManagementAPI/Controllers/UsersController.cs
@@ -0,0 +1,56 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using UserManagementAPI.Data;
+using UserManagementAPI.Models;
+
+namespace UserManagementAPI.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class UsersController : ControllerBase
+    {
+
+        private readonly static UserRepository _userRepository = new UserRepository();
+
+        [HttpGet]
+        public ActionResult<IEnumerable<User>> Get() => Ok(_userRepository.GetAll());
+
+        [HttpGet("{id}")]
+        public ActionResult<User> Get(int id)
+        {
+            var user = _userRepository.GetById(id);
+            return user != null ? Ok(user) : NotFound();
+        }
+
+        [HttpPost]
+        public ActionResult<User> Post([FromBody] User user)
+        {
+            if (user == null)
+            {
+                return BadRequest();
+            }
+
+            _userRepository.Add(user);
+            return CreatedAtAction(nameof(Get), new { id = user.Id }, user);
+        }
+
+        [HttpPut("{id}")]
+        public IActionResult Put(int id, [FromBody] User user)
+        {
+            if (user == null || user.Id != id)
+            {
+                return BadRequest();
+            }
+
+            _userRepository.Update(user);
+            return NoContent();
+        }
+
+        [HttpDelete("{id}")]
+        public IActionResult Delete(int id)
+        {
+            _userRepository.Delete(id);
+            return NoContent();
+        }
+    }
+}
---
+++ Week1/Day4/UserManagementAPI/Data/UserRepository.cs
@@ -0,0 +1,48 @@
+﻿using UserManagementAPI.Models;
+
+namespace UserManagementAPI.Data
+{
+    public class UserRepository
+    {
+        private List<User> _users;
+
+        public UserRepository() {
+
+
+            _users = new List<User>
+        {
+            new User { Id = 1, Name = "Suri", Email = "suri@gmail.com" },
+            new User { Id = 2, Name = "Mouni", Email = "mouni@gmail.com" }
+        };
+        }
+
+        public List<User> GetAll() => _users;
+
+        public User GetById(int id) => _users.FirstOrDefault(u => u.Id == id);
+
+        public void Add(User user)
+        {
+            user.Id = _users.Count + 1;
+            _users.Add(user);
+        }
+
+        public void Update(User user)
+        {
+            var existingUser = GetById(user.Id);
+            if (existingUser != null)
+            {
+                existingUser.Name = user.Name;
+                existingUser.Email = user.Email;
+            }
+        }
+
+        public void Delete(int id)
+        {
+            var user = GetById(id);
+            if (user != null)
+            {
+                _users.Remove(user);
+            }
+        }
+    }
+}
