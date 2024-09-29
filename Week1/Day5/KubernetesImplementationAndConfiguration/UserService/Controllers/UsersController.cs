29db65c20188da251b9b77373ce94085705f6287
CodeAdmirer <codeadmirer@gmail.com>
2024-09-29 16:16:55 +0530

    Adding the UserSerive project base code

---
+++ Week1/Day5/KubernetesImplementationAndConfiguration/UserService/Controllers/UsersController.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace UserService.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class UsersController : ControllerBase
+    {
+        [HttpGet]
+        public IActionResult GetUsers()
+        {
+            var users = new[]
+            {
+                    new { Id = 1, Name = "Suri" },
+                    new { Id = 2, Name = "Mouni" }
+                };
+            return Ok(users);
+        }
+    }
+}
