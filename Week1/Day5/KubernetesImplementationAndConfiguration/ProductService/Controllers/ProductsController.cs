dac68a2100cab2c34addd6c0dba45e3ccf7b48aa
CodeAdmirer <codeadmirer@gmail.com>
2024-09-29 16:42:59 +0530

    Added the Product service base code

---
+++ Week1/Day5/KubernetesImplementationAndConfiguration/ProductService/Controllers/ProductsController.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace ProductService.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ProductsController : ControllerBase
+    {
+        [HttpGet]
+        public IActionResult GetProducts()
+        {
+            var products = new[]
+            {
+                      new { Id = 1, Name = "Dell Laptop" },
+                      new { Id = 2, Name = "Oneplus SmartPhone" }
+                  };
+            return Ok(products);
+        }
+    }
+}
